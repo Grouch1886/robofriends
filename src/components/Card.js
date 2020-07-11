48322d413897a4b3be82b5bf755dfb4c1bd0b0cf
glg <Franchez.galangfabregas@gmail.com>
2020-07-11 20:06:46 +0700

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = ({name, occupation, id}) => {
+const Card = ({name, email, id}) => {
 	return (
 		<div className='tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5'> {/*Tachyon*/}
 			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
 			<div>
 				<h2>{name}</h2>
-				<h3>{occupation}</h3>
+				<h3>{email}</h3>
 			</div>
 		</div>
 	);
